8d1c4c278e0d5c7a6b28365032be5cbb21e00c8c
Pinacolada611 <jorgea.smok@gmail.com>
2026-03-23 12:53:42 -0600

    se agrego la primera fvncion, static void EvaluarContenido()

--- Proyecto simvlacion de decisiones/Program.cs
+++ Proyecto simvlacion de decisiones/Program.cs
@@ -56,4 +56,36 @@ class Proyecto
             }
         } while (opcion != 5);
     }
+
+    static void EvaluarContenido()
+    {
+        Console.WriteLine("Ingrese el tipo de contenido. (1.pelicula, 2.serie, 3.documental, 4.evento en vivo): ");
+        int Tipo = ValidarRango(1, 4);
+
+        Console.WriteLine("Ingrese la duracion del contenido. (en mintos): ");
+        int Duracion = ValidarRango(1, 100000);
+
+        Console.WriteLine("Ingrese la clasificacion del contenido. (1.todo público, 2.+13, 3.+18): ");
+        int Clasificacion = ValidarRango(1, 3);
+
+        Console.WriteLine("Ingrese la hora programada. (0-23): ");
+        int Hora = ValidarRango(0, 23);
+
+        Console.WriteLine("Ingrese Nivel de Prodvccion. (1.bajo, 2.medio, 3.alto): ");
+        int Nivel = ValidarRango(1, 3);
+
+        TotalEvaluados++; 
+        Console.WriteLine("\nDatos registrados.");
+
+        bool cumpleTecnico = true;
+        string motivoRechazo = "";
+
+        if (Tipo == 1 && (Duracion < 60 || Duracion > 180)) { cumpleTecnico = false; motivoRechazo += "Duración película (60-180min). "; }
+        if (!cumpleTecnico)
+        {
+            Rechazados++;
+            Console.WriteLine($"\nDECISIÓN FINAL: RECHAZAR\nRazon: {motivoRechazo}");
+            return;
+        }
+    }
 }
