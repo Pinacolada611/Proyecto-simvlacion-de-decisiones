f6b969b2bf2e2abbf0d673ff711dd4ccc0069535
Pinacolada611 <jorgea.smok@gmail.com>
2026-03-23 14:07:16 -0600

    el codigo al 100%

--- Proyecto simvlacion de decisiones/Program.cs
+++ Proyecto simvlacion de decisiones/Program.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Security.Claims;
-
+﻿using System
 class Proyecto
 {
+    static int TotalEvaluados = 0, Publicados = 0, Rechazados = 0, Revisiones = 0;
+    static int ImpactoAlto = 0, ImpactoMedio = 0, ImpactoBajo = 0;
+
     static void Main()
     {
-        int TotalEvaluados = 0;
-        int Publicados = 0;
-        int Rechazados = 0;
-        int Revisiones = 0;
-        int ImpactoAlto = 0;
-        int ImpactoMedio = 0;
-        int ImpactoBajo = 0;
-        int PorcentajeAprovado = 0;
-
         Menu();
     }
 
@@ -22,8 +14,8 @@ class Proyecto
         int opcion = 0;
         do
         {
-            Console.Clear();
-            Console.WriteLine("---Menu--- ");
+            Console.Clear(); //El .Clear limpiara la pantalla en cada vuelta
+            Console.WriteLine("---Menu--- ");
             Console.WriteLine("1. Evaluar nuevo contenido ");
             Console.WriteLine("2. Mostrar reglas del sistema ");
             Console.WriteLine("3. Mostrar estadisticas de la sesion ");
@@ -35,102 +27,138 @@ class Proyecto
 
             switch (opcion)
             {
-                case 1:
-                    EvaluarContenido();
-                    break;
-                case 2:
-                    MostrarReglas();
-                    break;
-                case 3:
-                    MostrarEstadisticas();
-                    break;
-                case 4:
-                    ReiniciarEstadisticas();
-                    break;
-                case 5:
-                    Console.WriteLine("Saliendo del programa: ");
-                    Console.ReadKey();
-                    break;
+                case 1: EvaluarContenido(); break;
+                case 2: MostrarReglas(); break;
+                case 3: MostrarEstadisticas(); break;
+                case 4: ReiniciarEstadisticas(); break;
+                case 5: ResumenFinal(); break;
             }
-
             if (opcion != 5)
             {
-                Console.WriteLine("\nEscoja una opcion para continuar: ");
+                Console.WriteLine("\nPresione una tecla para continuar: ");
+                Console.ReadKey();
             }
         } while (opcion != 5);
     }
 
     static void EvaluarContenido()
     {
+        Console.Clear();
         Console.WriteLine("Ingrese el tipo de contenido. (1.pelicula, 2.serie, 3.documental, 4.evento en vivo): ");
         int Tipo = ValidarRango(1, 4);
-        Console.WriteLine("Ingrese la duracion del contenido. (en mintos): ");
+        Console.WriteLine("Ingrese la duracion del contenido. (en minutos): ");
         int Duracion = ValidarRango(1, 100000);
-        Console.WriteLine("Ingrese la clasificacion del contenido. (1.todo público, 2.+13, 3.+18): ");
+        Console.WriteLine("Ingrese la clasificacion del contenido. (1.todo publico, 2.+13, 3.+18): ");
         int Clasificacion = ValidarRango(1, 3);
-        Console.WriteLine("Ingrese la hora programada. (0-23): ");
-        int Hora = ValidarRango(1, 23);
-        Console.WriteLine("Ingrese Nivel de Prodvccion. (1.bajo, 2.medio, 3.alto): ");
+        Console.WriteLine("Ingrese la hora programada. (0–23): ");
+        int Hora = ValidarRango(0, 23);
+        Console.WriteLine("Ingrese Nivel de Produccion. (1.bajo, 2.medio, 3.alto): ");
         int Nivel = ValidarRango(1, 3);
 
         TotalEvaluados++;
         Console.WriteLine("\nDatos registrados. ");
 
-        bool cumpleTecnico = true;
-        string motivoRechazo = "";
+        //a) Validación tecnica
+        bool ValTec = true;
+        string RechaMotiv = "";
 
-        if (Tipo == 1 && (Duracion < 60 || Duracion > 180)) { cumpleTecnico = false; motivoRechazo += "Duración película (60-180min). "; }
-        if (Tipo == 2 && (Duracion < 20 || Duracion > 90)) { cumpleTecnico = false; motivoRechazo += "Duración serie (20-90min). "; }
-        if (Tipo == 3 && (Duracion < 30 || Duracion > 120)) { cumpleTecnico = false; motivoRechazo += "Duración documental (30-120min). "; }
-        if (Tipo == 4 && (Duracion < 30 || Duracion > 240)) { cumpleTecnico = false; motivoRechazo += "Duración evento (30-240min). "; }
+        if (Tipo == 1 && (Duracion < 60 || Duracion > 180)) { ValTec = false; RechaMotiv += "Duracion pelicula (60-180min). "; }
+        if (Tipo == 2 && (Duracion < 20 || Duracion > 90)) { ValTec = false; RechaMotiv += "Duracion serie (20-90min). "; }
+        if (Tipo == 3 && (Duracion < 30 || Duracion > 120)) { ValTec = false; RechaMotiv += "Duracion documental (30-120min). "; }
+        if (Tipo == 4 && (Duracion < 30 || Duracion > 240)) { ValTec = false; RechaMotiv += "Duracion evento (30-240min). "; }
 
-        if (Clasif == 2 && (Hora < 6 || Hora > 22)) { cumpleTecnico = false; motivoRechazo += "Horario +13 (6-22h). "; }
-        if (Clasif == 3 && (Hora >= 5 && Hora < 22)) { cumpleTecnico = false; motivoRechazo += "Horario +18 (22-5h). "; }
+        // Reglas de horario
+        if (Clasificacion == 2 && (Hora < 6 || Hora > 22)) { ValTec = false; RechaMotiv += "Horario +13 (6-22h). "; }
+        // Para +18 (22 a 5), el contenido se RECHAZA si está fuera de ese rango (o sea, si es entre las 5 y las 22)
+        if (Clasificacion == 3 && (Hora >= 5 && Hora < 22)) { ValTec = false; RechaMotiv += "Horario +18 (22-5h). "; }
 
-        if (Nivel == 1 && Clasif == 3) { cumpleTecnico = false; motivoRechazo += "Producción baja no apta para +18. "; }
+        // Produccion
+        if (Nivel == 1 && Clasificacion == 3) { ValTec = false; RechaMotiv += "Produccion baja no apta para +18. "; }
 
-        if (!cumpleTecnico)
+        if (!ValTec)
         {
             Rechazados++;
-            Console.WriteLine($"\nDECISIÓN FINAL: RECHAZAR\nRazon: {motivoRechazo}");
+            Console.WriteLine($"\nDECISION FINAL: RECHAZAR\nRazon: {RechaMotiv}");
             return;
         }
 
-        string impacto = "Bajo";
-        if (Nivel == 3 || duracion > 120 || (hora >= 20 && hora <= 23)) { impacto = "Alto"; ImpactoAlto++; }
-        else if (nivel == 2 || (duracion >= 60 && duracion <= 120)) { impacto = "Medio"; ImpactoMedio++; }
+        // b) Clasificación de impacto
+        string impacto = "Bajo";
+        if (Nivel == 3 || Duracion > 120 || (Hora >= 20 && Hora <= 23)) { impacto = "Alto"; ImpactoAlto++; }
+        else if (Nivel == 2 || (Duracion >= 60 && Duracion <= 120)) { impacto = "Medio"; ImpactoMedio++; }
         else { ImpactoBajo++; }
 
-        if (impacto == "Alto")
+        // c) Decisión final
+        if (impacto == "Alto")
         {
             Revisiones++;
-            Console.WriteLine("\nDECISIÓN FINAL: ENVIAR A REVISIÓN");
+            Console.WriteLine("\ndecision final enviar revision. ");
             Console.WriteLine("Razon: Cumple reglas técnicas pero tiene Impacto Alto.");
         }
         else
         {
             Publicados++;
-            Console.WriteLine($"\ndecision final: publicar (Impacto {impacto})")
+            Console.WriteLine($"\ndecision final: publicar (Impacto {impacto})");
             Console.WriteLine("Razon: Cumple reglas tecnicas e impacto no es Alto.");
         }
+    }
+
+    static int ValidarRango(int min, int max)
+    {
+        int valor;
+        while (!int.TryParse(Console.ReadLine(), out valor) || valor < min || valor > max)
+        {
+            Console.Write($"Entrada Invalida. Ingrese numero entre {min} y {max}: ");
+        }
+        return valor;
     }
 
     static void MostrarReglas()
     {
         Console.Clear();
-        Console.WriteLine("Reglas de timpo.");
-        Console.WriteLine("Película: 60-180 minutos.");
-        Console.WriteLine("Serie: 20-90 minutos.");
-        Console.WriteLine("Documental: 30-120 minutos.");
-        Console.WriteLine("Evento en vivo: 30-240 minutos.");
-
-        Console.Clear();
-        Console.WriteLine("Todo público: cualquier hora.");
+        Console.WriteLine("Reglas de tiempo.");
+        Console.WriteLine("Pelicula: 60–180 minutos.");
+        Console.WriteLine("Serie: 20–90 minutos.");
+        Console.WriteLine("Documental: 30–120 minutos.");
+        Console.WriteLine("Evento en vivo: 30–240 minutos.");
+        Console.WriteLine("\nReglas de horario.");
+        Console.WriteLine("Todo publico: cualquier hora.");
         Console.WriteLine("+13: entre 6 y 22 horas.");
         Console.WriteLine("+18: entre 22 y 5 horas.");
+        Console.WriteLine("\nProducción.");
+        Console.WriteLine("Produccion baja ( Todo publico o +13) ");
+        Console.WriteLine("Producción media o alta válida (Cualquier clasificación) ");
+    }
 
+    static void MostrarEstadisticas()
+    {
         Console.Clear();
-        Console.WriteLine("Producción baja ( Todo público o +13) ");
-        Console.WriteLine("Producción media o alta válida ( Cualquier clasificación) ");
+        Console.WriteLine($"Total Evaluados: {TotalEvaluados}");
+        Console.WriteLine($"Publicados: {Publicados}");
+        Console.WriteLine($"Rechazados: {Rechazados}");
+        Console.WriteLine($"En Revision: {Revisiones}");
+
+        if (TotalEvaluados > 0)
+        {
+            double porc = (double)Publicados / TotalEvaluados * 100;
+            Console.WriteLine($"Porcentaje de Aprobacion: {porc:F2}%");
+        }
+    }
+
+    static void ReiniciarEstadisticas()
+    {
+        TotalEvaluados = 0; Publicados = 0; Rechazados = 0; Revisiones = 0;
+        ImpactoAlto = 0; ImpactoMedio = 0; ImpactoBajo = 0;
+        Console.WriteLine("\nEstadisticas reiniciadas. ");
+    }
+
+    static void ResumenFinal()
+    {
+        Console.Clear();
+        Console.WriteLine("---resumen---");
+        MostrarEstadisticas();
+        Console.WriteLine("\nSaliendo...");
+        Console.ReadKey();
     }
 }
+//este seria todo el simulador.
